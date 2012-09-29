ae3816ccf9f73c513c93320f610f18bf9b6fd3e9
Cary Landholt <cary@landholt.com>
2012-09-29 17:50:29 -0500

    using replaced grunt-less with grunt-contrib-less

--- grunt.js
+++ grunt.js
@@ -145,7 +145,7 @@ module.exports = function (grunt) {
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-less');
+	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-hustler');
 	grunt.registerTask('core', 'delete coffeeLint coffee copy lint less');
 	grunt.registerTask('bootstrap', 'core template:dev');
