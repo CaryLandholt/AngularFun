d370ed554e03f673376c9a9dacc95f38a8a0ce39
Cary Landholt <cary@landholt.com>
2012-12-24 19:19:07 -0600

    initial stab at improving the documentation

--- grunt.js
+++ grunt.js
@@ -1,11 +1,11 @@
-/*global module*/
-
 module.exports = function (grunt) {
-	'use strict';
-
 	grunt.initConfig({
-		pkg: '<json:package.json>',
-
+		/*
+			deletes dist and temp directories
+			the temp directory is used during the build process
+			the dist directory contains the artifacts of the build
+			these directories should be deleted before subsequent builds
+		*/
 		delete: {
 			reset: {
 				files: ['./dist/']
@@ -15,6 +15,9 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			contains linting rules for CoffeeScript files
+		*/
 		coffeeLint: {
 			scripts: {
 				files: ['./src/scripts/**/*.coffee'],
@@ -31,19 +34,16 @@ module.exports = function (grunt) {
 			},
 			tests: {
 				files: ['./test/scripts/**/*.coffee'],
-				indentation: {
-					value: 1,
-					level: 'error'
-				},
-				max_line_length: {
-					level: 'ignore'
-				},
-				no_tabs: {
-					level: 'ignore'
-				}
+				indentation: '<config:coffeeLint.scripts.indentation>',
+				max_line_length: '<config:coffeeLint.scripts.max_line_length>',
+				no_tabs: '<config:coffeeLint.scripts.no_tabs>',
 			}
 		},
 
+
+		/*
+			compiles CoffeeScript (.coffee) files to JavaScript (.js)
+		*/
 		coffee: {
 			scripts: {
 				files: {
@@ -59,6 +59,9 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			compiles LESS (.less) files to CSS (.css)
+		*/
 		less: {
 			styles: {
 				files: {
@@ -67,6 +70,22 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			compiles template files (.template) to HTML (.html)
+
+			.template files are essentially html; however, you can take advantage
+			of features provided by grunt such as underscore templating
+
+			the example below demonstrates the use of the environment attribute
+			in 'prod' the concatenated and minified scripts are used along with a unique QueryString parameter to address browser caching
+			in environments other than 'prod' the individual files are used and loaded with RequireJS
+
+			<% if (config.environment === 'prod') { %>
+				<script src="/scripts/scripts.min.js?_=v<%= config.uniqueVersion() %>"></script>
+			<% } else { %>
+				<script data-main="/scripts/main.js" src="/scripts/libs/require.js"></script>
+			<% } %>
+		*/
 		template: {
 			views: {
 				files: {
@@ -85,6 +104,49 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			creates a single file consisting of multiple html (views) files surrounded by script tags
+
+			for example, take the following two files,
+			/temp/views/people.html (compiled from /src/views/people.template)
+			<ul ng-hide="!people.length">
+				<li class="row" ng-repeat="person in people | orderBy:'name'">
+					<a ng-href="#/people/{{person.id}}" ng-bind="person.name"></a>
+				</li>
+			</ul>
+
+			/temp/views/repos.html (compiled from /src/views/repos.html)
+			<ul ng-hide="!repos.length">
+				<li ng-repeat="repo in repos | orderBy:'pushed_at':true">
+					<a ng-href="{{repo.url}}" ng-bind="repo.name" target="_blank"></a>
+					<div ng-bind="repo.description"></div>
+				</li>
+			</ul>
+
+			AngularJS will interpret inlined scripts with type of "text/ng-template" in lieu of retrieving the view from the server
+			the id of the script tag must match the path requested
+			since the path includes the temp directory, this must be trimmed
+
+			the output of the configuration below is:
+			/temp/views/views.html
+			<script id="/views/people.html" type="text/ng-template">
+				<ul ng-hide="!people.length">
+					<li class="row" ng-repeat="person in people | orderBy:'name'">
+						<a ng-href="#/people/{{person.id}}" ng-bind="person.name"></a>
+					</li>
+				</ul>
+			</script>
+			<script id="/views/repos.html" type="text/ng-template">
+				<ul ng-hide="!repos.length">
+					<li ng-repeat="repo in repos | orderBy:'pushed_at':true">
+						<a ng-href="{{repo.url}}" ng-bind="repo.name" target="_blank"></a>
+						<div ng-bind="repo.description"></div>
+					</li>
+				</ul>
+			</script>
+
+			now the views.html file can be included in the application and avoid making requests to the server for the templates
+		*/
 		inlineTemplate: {
 			views: {
 				files: {
@@ -95,18 +157,27 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			copies directories and files from one location to another
+		*/
 		copy: {
+			// copies libs and img directories to temp
 			temp: {
 				files: {
 					'./temp/scripts/libs/': './src/scripts/libs/',
 					'./temp/img/': './src/img/'
 				}
 			},
+			// copies the contents of the temp directory to the dist directory
+			// in 'dev' individual files are used
 			dev: {
 				files: {
 					'./dist/': './temp/'
 				}
 			},
+			// copies select files from the temp directory to the dist directory
+			// in 'prod' minified files are used along with img and libs
+			// the dist artifacts contain only the files necessary to run the application
 			prod: {
 				files: {
 					'./dist/img/': './temp/img/',
@@ -116,21 +187,25 @@ module.exports = function (grunt) {
 					'./dist/index.html': './temp/index.min.html'
 				}
 			},
+			// task is run when a watched script is modified
 			scripts: {
 				files: {
 					'./dist/scripts/': './temp/scripts/'
 				}
 			},
+			// task is run when a watched style is modified
 			styles: {
 				files: {
 					'./dist/styles/': './temp/styles/'
 				}
 			},
+			// task is run when the watched index.template file is modified
 			index: {
 				files: {
 					'./dist/': './temp/index.html'
 				}
 			},
+			// task is run when a watched view is modified
 			views: {
 				files: {
 					'./dist/views/': './temp/views/'
@@ -138,6 +213,14 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			RequireJS optimizer configuration for both scripts and styles
+			this configuration is only used in the 'prod' build
+			the optimizer will scan the main file, walk the dependency tree, and write the output in dependent sequence to a single file
+			since RequireJS is not being used outside of the main file, RequireJS is not needed for final output and is excluded
+			RequireJS is still used for the 'dev' build
+			the main file is used only to establish the proper loading sequence
+		*/
 		requirejs: {
 			scripts: {
 				baseUrl: './temp/scripts/',
@@ -145,6 +228,7 @@ module.exports = function (grunt) {
 				logLevel: 0,
 				mainConfigFile: './temp/scripts/main.js',
 				name: 'main',
+				// exclude main from the final output to avoid the dependency on RequireJS at runtime
 				onBuildWrite: function (moduleName, path, contents) {
 					var modulesToExclude = ['main'],
 						shouldExcludeModule = modulesToExclude.indexOf(moduleName) >= 0;
@@ -172,6 +256,13 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			minifies index.html
+			extra white space and comments will be removed
+			content within <pre /> tags will be left unchanged
+			IE conditional comments will be left unchanged
+			as of this writing, the output is reduced by over 14%
+		*/
 		minifyHtml: {
 			prod: {
 				files: {
@@ -180,6 +271,9 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			sets up file watchers and runs tasks when watched files are changed
+		*/
 		watch: {
 			scripts: {
 				files: './src/scripts/**/*.coffee',
@@ -199,6 +293,11 @@ module.exports = function (grunt) {
 			}
 		},
 
+		/*
+			runs a web server at the specified port
+			can optionally watch for changes to the file referenced in the watch setting
+			the web server will automatically restart once the changes have been saved
+		*/
 		server: {
 			app: {
 				src: './server.coffee',
@@ -207,8 +306,14 @@ module.exports = function (grunt) {
 			}
 		},
 
-		// LiveReload
-		// http://help.livereload.com/kb/general-use/browser-extensions
+		/*
+			leverages the LiveReload browser plugin to automatically reload the browser when watched files have changed
+
+			as of this writing, Chrome, Firefox, and Safari are supported
+
+			get the plugin:
+			here http://help.livereload.com/kb/general-use/browser-extensions
+		*/
 		reload: {
 			liveReload: true,
 			port: 35729
